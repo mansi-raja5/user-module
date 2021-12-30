06141a44a289114e9b6b3df392b37612ebf91993
Mansi Raja <mansi.raja@encora.com>
2021-12-30 19:50:34 +0530

    Restructuring files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import UserForm from "./components/NewUser/UserForm";
+import UserForm from "./components/Users/UserForm";
 import UserList from "./components/Users/UserList";
 
 function App() {
   const [userData, setUserData] = useState([]);
   const addUserHandler = (enteredUserData) => {
     setUserData((prevUserData) => {
-      return [enteredUserData,...prevUserData];
+      return [enteredUserData, ...prevUserData];
     });
   };
   console.log(userData);
