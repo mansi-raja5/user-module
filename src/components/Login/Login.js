d72d4d9553d73e42e0dfd3d11d795b84f66b0fb2
Mansi Raja <mansi.raja@encora.com>
2022-01-02 01:13:48 +0530

    Remove console logs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,6 @@ import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./Login.module.css";
 
-let counter = 0;
 const Login = (props) => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
@@ -16,50 +15,13 @@ const Login = (props) => {
   };
 
   useEffect(() => {
-    console.log("I'll run on every state change!");
-
-    return () => {
-      console.log(
-        "Effect Cleanup! I'll trigger on every state change, before useEffect runs but not for the first time!"
-      );
-    };
-  });
-
-  useEffect(() => {
-    console.log("I'll run only once!");
-
-    return () => {
-      console.log(
-        "Effect Cleanup! I'll trigger when the component is removed from the DOM!"
-      );
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log(
-      "I'll run only when enteredEmail variable changes its state and component re-evaluates!"
-    );
-    return () => {
-      console.log(
-        "Effect Cleanup! I'll trigger when enteredEmail variable changes its state, before useEffects but not for the first time!"
-      );
-    };
-  }, [enteredEmail]);
-
-  useEffect(() => {
-    console.log("inside effect ");
     const identifier = setTimeout(() => {
-      console.log("checking form valifity!");
-      console.log(counter++);
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
       );
     }, 5000);
 
     return () => {
-      console.log(
-        "clean up! This will be executed once effect is executed previously! so this will start from second useeffect execution!"
-      );
       clearTimeout(identifier);
     };
   }, [enteredEmail, enteredPassword]);
